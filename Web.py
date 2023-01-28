8835679abdccd673c7938895587789f5568779f4
Costin98 <morun.costin@yahoo.com>
2023-01-28 17:09:39 +0200

    Final commit

--- Web.py
+++ Web.py
@@ -1,29 +1,28 @@
 
-import streamlit as st
+import streamlit as gl
 import functions
 
 todos = functions.get_todos()
-# https://github.com/CostinM98/repository/blob/master/WebAppNo.1.py
-# https://github.com/CostinM98/repository/master/WebAppNo.1
+
 def add_todo():
-    todo_local = st.session_state["new_todo"] + '\n'
+    todo_local = gl.session_state["new_todo"] + '\n'
     todos.append(todo_local)
     functions.writeTodos(todos)
 
 
 
-st.title('My Todo APP')
-st.write("This app is to increase your productivity")
+gl.title('My Todo APP')
+gl.write("This app is to increase your productivity")
 
-st.text_input(label='', placeholder='Add a new todo...',
+gl.text_input(label='', placeholder='Add a new todo...',
               on_change=add_todo, key='new_todo')
 
 for index, todo in enumerate(todos):
-    checkbox = st.checkbox(todo, key=f'checkbox_{index}')
+    checkbox =gl.checkbox(todo, key=f'checkbox_{index}')
     if checkbox:
         todos.pop(index)
         functions.writeTodos(todos)
-        del st.session_state[f'checkbox_{index}']
-        st.experimental_rerun()
+        del gl.session_state[f'checkbox_{index}']
+        gl.experimental_rerun()
 
 
